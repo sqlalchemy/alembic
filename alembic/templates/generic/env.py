11549d1f4c1889a1b72a1391e6ee41d67b47cdbf
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2012-02-22 23:53:10 +0100

    fix spelling

--- alembic/templates/generic/env.py
+++ alembic/templates/generic/env.py
@@ -7,7 +7,7 @@ from logging.config import fileConfig
 # access to the values within the .ini file in use.
 config = context.config
 
-# Interpret the config file for Pyhton logging. 
+# Interpret the config file for Python logging. 
 # This line sets up loggers basically.
 fileConfig(config.config_file_name)
 
