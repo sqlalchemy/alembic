36b191f1f7649edf5cd72e4e41b42e34eed0d259
Mike Bayer <mike_mp@zzzcomputing.com>
2013-12-18 11:58:30 -0500

    since msg() right-trims, move the space to the subsequent message

--- alembic/util.py
+++ alembic/util.py
@@ -151,13 +151,13 @@ def coerce_resource_to_filename(fname):
     return fname
 
 def status(_statmsg, fn, *arg, **kw):
-    msg(_statmsg + " ... ", False)
+    msg(_statmsg + " ...", False)
     try:
         ret = fn(*arg, **kw)
-        write_outstream(sys.stdout, "done\n")
+        write_outstream(sys.stdout, " done\n")
         return ret
     except:
-        write_outstream(sys.stdout, "FAILED\n")
+        write_outstream(sys.stdout, " FAILED\n")
         raise
 
 def err(message):
