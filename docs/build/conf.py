43e525c0d7c1c259f1438b40f460cf60d737b672
Mike Bayer <mike_mp@zzzcomputing.com>
2013-12-30 12:23:46 -0500

    try out paramlinks

--- docs/build/conf.py
+++ docs/build/conf.py
@@ -31,7 +31,7 @@ import alembic
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx',
-                'changelog']
+                'changelog', 'sphinx_paramlinks']
 
 # tags to sort on inside of sections
 changelog_sections = ["feature", "bug", "moved", "changed", "removed"]
