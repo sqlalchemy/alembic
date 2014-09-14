694b1135a066d1af3c700fd6284a9028643b35bd
Mike Bayer <mike_mp@zzzcomputing.com>
2014-09-14 11:51:04 -0400

    fixes for the nose runner

--- alembic/testing/plugin/noseplugin.py
+++ alembic/testing/plugin/noseplugin.py
@@ -61,7 +61,7 @@ class NoseSQLAlchemy(Plugin):
         plugin_base.set_coverage_flag(options.enable_plugin_coverage)
 
         global fixtures
-        from sqlalchemy.testing import fixtures
+        from alembic.testing import fixtures
 
     def begin(self):
         plugin_base.post_begin()
