6d797a813818171d9bdf0c1e1113c1b7a3475042
Mike Bayer <mike_mp@zzzcomputing.com>
2015-07-06 21:24:31 -0400

    - api.py is tiny now, don't need separate generate.py module

--- alembic/autogenerate/__init__.py
+++ alembic/autogenerate/__init__.py
@@ -1,7 +1,7 @@
 from .api import ( # noqa
     compare_metadata, _render_migration_diffs,
-    produce_migrations, render_python_code
+    produce_migrations, render_python_code,
+    RevisionContext
     )
 from .compare import _produce_net_changes  # noqa
-from .generate import RevisionContext  # noqa
 from .render import render_op_text, renderers  # noqa
