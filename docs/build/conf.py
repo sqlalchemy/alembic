a18f34708195e51f6d029ac9f6731c424f7b3fdb
Mike Bayer <mike_mp@zzzcomputing.com>
2016-01-29 11:27:00 -0500

    - happy new year

--- docs/build/conf.py
+++ docs/build/conf.py
@@ -63,7 +63,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Alembic'
-copyright = u'2010-2015, Mike Bayer'
+copyright = u'2010-2016, Mike Bayer'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
