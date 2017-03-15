d4ac63194a986be3e0358b894c8fa3d465bc0b7b
Mike Bayer <mike_mp@zzzcomputing.com>
2017-03-15 11:54:55 -0400

    - add custom sidebar file
    Change-Id: I03078ee419882942a5891c90cef18851b53f905b

--- docs/build/conf.py
+++ docs/build/conf.py
@@ -121,6 +121,7 @@ html_theme = 'nature'
 
 html_style = "nature_override.css"
 
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
@@ -159,7 +160,7 @@ html_static_path = ['_static']
 #html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-#html_sidebars = {}
+html_sidebars = {"**": ["site_custom_sidebars.html", "localtoc.html", "searchbox.html", "relations.html"]}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
