ab8e9dd3826871ddaa18b46d08b0149ea52abb76
Mike Bayer <mike_mp@zzzcomputing.com>
2019-01-13 12:44:52 -0500

    - happy new year
    Change-Id: I7ee1ea57bc4a753155983ced0559ca81797f7531

--- docs/build/conf.py
+++ docs/build/conf.py
@@ -62,7 +62,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Alembic'
-copyright = u'2010-2018, Mike Bayer'
+copyright = u'2010-2019, Mike Bayer'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
