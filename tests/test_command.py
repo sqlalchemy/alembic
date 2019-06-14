689f45b9aa618f432a804b02aefc11f2294cb0c5
Aviskar KC <aviskarkc10@gmail.com>
2019-06-14 17:27:28 +0545

    Add test for initializing migrations folder when folder exists and is not empty

--- tests/test_command.py
+++ tests/test_command.py
@@ -798,3 +798,15 @@ class CommandLineTest(TestBase):
                             # not too long
                             assert len(help_text) < 80
         assert not commands, "Commands without help text: %s" % commands
+    
+    @mock.patch("alembic.command.os.listdir", return_value = ['file1', 'file2'])
+    @mock.patch("alembic.command.os.access", return_value = False)
+    def test_init_file_exists_and_is_not_empty(self, mocked_listdir, mocked_access):
+        directory = 'alembic'
+        assert_raises_message(
+            util.CommandError,
+            "Directory %s already exists and is not empty" % directory,
+            command.init,
+            self.cfg,
+            directory = directory
+        )
