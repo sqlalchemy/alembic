27a902cde7d6f06a33dcffed93fdb89d8f7bc3d7
Mike Bayer <mike_mp@zzzcomputing.com>
2021-04-17 13:21:36 -0400

    next release is 1.6.0
    The change for #803 as well as the upcoming fix for
    issue #765 are major changes to the internals, so we will be
    on a new minor version.
    
    Change-Id: I6c384c4900761d9b4fb27742a4c8ecb227aa87e3

--- alembic/__init__.py
+++ alembic/__init__.py
@@ -5,7 +5,7 @@ from . import op  # noqa
 from .runtime import environment
 from .runtime import migration
 
-__version__ = "1.5.9"
+__version__ = "1.6.0"
 
 sys.modules["alembic.migration"] = migration
 sys.modules["alembic.environment"] = environment
