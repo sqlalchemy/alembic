d11a51d902134bbab0eccf9abcdfb629d9794067
Mike Bayer <mike_mp@zzzcomputing.com>
2021-05-03 14:48:08 -0400

    Version 1.6.1 placeholder

--- alembic/__init__.py
+++ alembic/__init__.py
@@ -5,7 +5,7 @@ from . import op  # noqa
 from .runtime import environment
 from .runtime import migration
 
-__version__ = "1.6.0"
+__version__ = "1.6.1"
 
 sys.modules["alembic.migration"] = migration
 sys.modules["alembic.environment"] = environment
--- docs/build/changelog.rst
+++ docs/build/changelog.rst
@@ -4,6 +4,10 @@ Changelog
 ==========
 
 .. changelog::
+    :version: 1.6.1
+    :include_notes_from: unreleased
+
+.. changelog::
     :version: 1.6.0
     :released: May 3, 2021
 
