edd64ea2a322049a4a536c1a9ae8e498db73be23
Mike Bayer <mike_mp@zzzcomputing.com>
2022-11-28 15:54:11 -0500

    add width + overflow to keep toc from flowing out of sidebar
    also add sphinx-autobuild to the makefile
    
    Change-Id: I7abd08cefe98d6b57394fad20bd06c3e0b928465
    References: https://github.com/sphinx-doc/sphinx/issues/11000
    (cherry picked from commit 9eafd483018775d9e1144512ed820ba2a80d2e61)

--- docs/build/conf.py
+++ docs/build/conf.py
@@ -150,6 +150,7 @@ html_style = "nature_override.css"
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
+# NOTE: use nature_override.css, not this
 # html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
