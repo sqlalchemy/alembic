e3055cd5baad328641366026af66639f333c520c
Mike Bayer <mike_mp@zzzcomputing.com>
2023-01-03 12:51:53 -0500

    happy new year 2023
    Change-Id: Ia163c800e074443b92cd59c786ac09073267c271

--- docs/build/conf.py
+++ docs/build/conf.py
@@ -86,7 +86,7 @@ master_doc = "index"
 
 # General information about the project.
 project = "Alembic"
-copyright = "2010-2022, Mike Bayer"  # noqa
+copyright = "2010-2023, Mike Bayer"  # noqa
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
