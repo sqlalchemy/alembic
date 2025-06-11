8c0f4176c001bd9f594430ce8f4099b800e9a0c3
jonathan vanasco <jonathan@2xlp.com>
2025-06-11 15:51:52 -0400

    simplified append

--- alembic/command.py
+++ alembic/command.py
@@ -152,7 +152,6 @@ def init(
                     file_path,
                     toml_path,
                     script_location=toml_script_location_directory,
-                    append_delimiter=b"\n\n",
                 )
             else:
                 script._generate_template(
