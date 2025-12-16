f0a0c1a913b93a2c71881e69be83a7888e21bc5e
Daniël <93520871+Flaab123@users.noreply.github.com>
2025-12-16 09:23:57 +0100

    Change type hint for partial_reordering parameter

--- alembic/operations/base.py
+++ alembic/operations/base.py
@@ -248,7 +248,7 @@ class AbstractOperations(util.ModuleClsProxy):
         table_name: str,
         schema: Optional[str] = None,
         recreate: Literal["auto", "always", "never"] = "auto",
-        partial_reordering: Optional[List[Tuple[Any, ...]]] = None,
+        partial_reordering: list[tuple[str, ...]] | None = None,
         copy_from: Optional[Table] = None,
         table_args: Tuple[Any, ...] = (),
         table_kwargs: Mapping[str, Any] = util.immutabledict(),
