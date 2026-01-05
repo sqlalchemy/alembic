sqlalchemy/alembic

commit e532a7e39cb6b0e91fbe045778f31a997ded452d

organize into a "plugin" directory structure
we attempt to move autogen functions into independent units
that are more obviously pluggable, and we add support for
arbitrary "plugin" entrypoints that could add more pluggable
units into autogenerate or anywhere else

Change-Id: Id606a76dc6d12a308028f6cfdad690e0e63a43e5

alembic/autogenerate/compare/server_defaults.py
@@ -1,0 +1,339 @@
+from __future__ import annotations
+
+import logging
+import re
+from types import NoneType
+from typing import Any
+from typing import cast
+from typing import Optional
+from typing import Sequence
+from typing import TYPE_CHECKING
+from typing import Union
+
+from sqlalchemy import schema as sa_schema
+from sqlalchemy.sql.schema import DefaultClause
+
+from ... import util
+from ...util import DispatchPriority
+from ...util import PriorityDispatchResult
+from ...util import sqla_compat
+
+if TYPE_CHECKING:
+    from sqlalchemy.sql.elements import quoted_name
+    from sqlalchemy.sql.schema import Column
+
+    from ...autogenerate.api import AutogenContext
+    from ...operations.ops import AlterColumnOp
+    from ...runtime.plugins import Plugin
+
+log = logging.getLogger(__name__)
+
+
+def _render_server_default_for_compare(
+    metadata_default: Optional[Any], autogen_context: AutogenContext
+) -> Optional[str]:
+    if isinstance(metadata_default, sa_schema.DefaultClause):
+        if isinstance(metadata_default.arg, str):
+            metadata_default = metadata_default.arg
+        else:
+            metadata_default = str(
+                metadata_default.arg.compile(
+                    dialect=autogen_context.dialect,
+                    compile_kwargs={"literal_binds": True},
+                )
+            )
+    if isinstance(metadata_default, str):
+        return metadata_default
+    else:
+        return None
+
+
+def _normalize_computed_default(sqltext: str) -> str:
+    """we want to warn if a computed sql expression has changed.  however
+    we don't want false positives and the warning is not that critical.
+    so filter out most forms of variability from the SQL text.
+
+    """
+
+    return re.sub(r"[ \(\)'\"`\[\]\t\r\n]", "", sqltext).lower()
+
+
+def _compare_computed_default(
+    autogen_context: AutogenContext,
+    alter_column_op: AlterColumnOp,
+    schema: Optional[str],
+    tname: str,
+    cname: str,
+    conn_col: Column[Any],
+    metadata_col: Column[Any],
+) -> PriorityDispatchResult:
+
+    metadata_default = metadata_col.server_default
+    conn_col_default = conn_col.server_default
+    if conn_col_default is None and metadata_default is None:
+        return PriorityDispatchResult.CONTINUE
+
+    if sqla_compat._server_default_is_computed(
+        conn_col_default
+    ) and not sqla_compat._server_default_is_computed(metadata_default):
+        _warn_computed_not_supported(tname, cname)
+        return PriorityDispatchResult.STOP
+
+    if not sqla_compat._server_default_is_computed(metadata_default):
+        return PriorityDispatchResult.CONTINUE
+
+    rendered_metadata_default = str(
+        cast(sa_schema.Computed, metadata_col.server_default).sqltext.compile(
+            dialect=autogen_context.dialect,
+            compile_kwargs={"literal_binds": True},
+        )
+    )
+
+    # since we cannot change computed columns, we do only a crude comparison
+    # here where we try to eliminate syntactical differences in order to
+    # get a minimal comparison just to emit a warning.
+
+    rendered_metadata_default = _normalize_computed_default(
+        rendered_metadata_default
+    )
+
+    if isinstance(conn_col.server_default, sa_schema.Computed):
+        rendered_conn_default = str(
+            conn_col.server_default.sqltext.compile(
+                dialect=autogen_context.dialect,
+                compile_kwargs={"literal_binds": True},
+            )
+        )
+        rendered_conn_default = _normalize_computed_default(
+            rendered_conn_default
+        )
+    else:
+        rendered_conn_default = ""
+
+    if rendered_metadata_default != rendered_conn_default:
+        _warn_computed_not_supported(tname, cname)
+
+    return PriorityDispatchResult.STOP
+
+
+def _warn_computed_not_supported(tname: str, cname: str) -> None:
+    util.warn("Computed default on %s.%s cannot be modified" % (tname, cname))
+
+
+def _compare_identity_default(
+    autogen_context: AutogenContext,
+    alter_column_op: AlterColumnOp,
+    schema: Optional[str],
+    tname: Union[quoted_name, str],
+    cname: Union[quoted_name, str],
+    conn_col: Column[Any],
+    metadata_col: Column[Any],
+    skip: Sequence[str] = (
+        "order",
+        "on_null",
+        "oracle_order",
+        "oracle_on_null",
+    ),
+) -> PriorityDispatchResult:
+
+    metadata_default = metadata_col.server_default
+    conn_col_default = conn_col.server_default
+    if (
+        conn_col_default is None
+        and metadata_default is None
+        or not sqla_compat._server_default_is_identity(
+            metadata_default, conn_col_default
+        )
+    ):
+        return PriorityDispatchResult.CONTINUE
+
+    assert isinstance(
+        metadata_col.server_default,
+        (sa_schema.Identity, sa_schema.Sequence, NoneType),
+    )
+    assert isinstance(
+        conn_col.server_default,
+        (sa_schema.Identity, sa_schema.Sequence, NoneType),
+    )
+
+    impl = autogen_context.migration_context.impl
+    diff, _, is_alter = impl._compare_identity_default(  # type: ignore[no-untyped-call]  # noqa: E501
+        metadata_col.server_default, conn_col.server_default
+    )
+
+    if is_alter:
+        alter_column_op.modify_server_default = metadata_default
+        if diff:
+            log.info(
+                "Detected server default on column '%s.%s': "
+                "identity options attributes %s",
+                tname,
+                cname,
+                sorted(diff),
+            )
+
+            return PriorityDispatchResult.STOP
+
+    return PriorityDispatchResult.CONTINUE
+
+
+def _user_compare_server_default(
+    autogen_context: AutogenContext,
+    alter_column_op: AlterColumnOp,
+    schema: Optional[str],
+    tname: Union[quoted_name, str],
+    cname: Union[quoted_name, str],
+    conn_col: Column[Any],
+    metadata_col: Column[Any],
+) -> PriorityDispatchResult:
+
+    metadata_default = metadata_col.server_default
+    conn_col_default = conn_col.server_default
+    if conn_col_default is None and metadata_default is None:
+        return PriorityDispatchResult.CONTINUE
+
+    alter_column_op.existing_server_default = conn_col_default
+
+    migration_context = autogen_context.migration_context
+
+    if migration_context._user_compare_server_default is False:
+        return PriorityDispatchResult.STOP
+
+    if not callable(migration_context._user_compare_server_default):
+        return PriorityDispatchResult.CONTINUE
+
+    rendered_metadata_default = _render_server_default_for_compare(
+        metadata_default, autogen_context
+    )
+    rendered_conn_default = (
+        cast(Any, conn_col_default).arg.text if conn_col_default else None
+    )
+
+    is_diff = migration_context._user_compare_server_default(
+        migration_context,
+        conn_col,
+        metadata_col,
+        rendered_conn_default,
+        metadata_col.server_default,
+        rendered_metadata_default,
+    )
+    if is_diff:
+        alter_column_op.modify_server_default = metadata_default
+        log.info(
+            "User defined function %s detected "
+            "server default on column '%s.%s'",
+            migration_context._user_compare_server_default,
+            tname,
+            cname,
+        )
+        return PriorityDispatchResult.STOP
+    return PriorityDispatchResult.CONTINUE
+
+
+def _dialect_impl_compare_server_default(
+    autogen_context: AutogenContext,
+    alter_column_op: AlterColumnOp,
+    schema: Optional[str],
+    tname: Union[quoted_name, str],
+    cname: Union[quoted_name, str],
+    conn_col: Column[Any],
+    metadata_col: Column[Any],
+) -> PriorityDispatchResult:
+    """use dialect.impl.compare_server_default.
+
+    This would in theory not be needed.  however we dont know if any
+    third party libraries haven't made their own alembic dialect and
+    implemented this method.
+
+    """
+    metadata_default = metadata_col.server_default
+    conn_col_default = conn_col.server_default
+    if conn_col_default is None and metadata_default is None:
+        return PriorityDispatchResult.CONTINUE
+
+    # this is already done by _user_compare_server_default,
+    # but doing it here also for unit tests that want to call
+    # _dialect_impl_compare_server_default directly
+    alter_column_op.existing_server_default = conn_col_default
+
+    if not isinstance(
+        metadata_default, (DefaultClause, NoneType)
+    ) or not isinstance(conn_col_default, (DefaultClause, NoneType)):
+        return PriorityDispatchResult.CONTINUE
+
+    migration_context = autogen_context.migration_context
+
+    rendered_metadata_default = _render_server_default_for_compare(
+        metadata_default, autogen_context
+    )
+    rendered_conn_default = (
+        cast(Any, conn_col_default).arg.text if conn_col_default else None
+    )
+
+    is_diff = migration_context.impl.compare_server_default(  # type: ignore[no-untyped-call]  # noqa: E501
+        conn_col,
+        metadata_col,
+        rendered_metadata_default,
+        rendered_conn_default,
+    )
+    if is_diff:
+        alter_column_op.modify_server_default = metadata_default
+        log.info(
+            "Dialect impl %s detected server default on column '%s.%s'",
+            migration_context.impl,
+            tname,
+            cname,
+        )
+        return PriorityDispatchResult.STOP
+    return PriorityDispatchResult.CONTINUE
+
+
+def _setup_autoincrement(
+    autogen_context: AutogenContext,
+    alter_column_op: AlterColumnOp,
+    schema: Optional[str],
+    tname: Union[quoted_name, str],
+    cname: quoted_name,
+    conn_col: Column[Any],
+    metadata_col: Column[Any],
+) -> PriorityDispatchResult:
+    if metadata_col.table._autoincrement_column is metadata_col:
+        alter_column_op.kw["autoincrement"] = True
+    elif metadata_col.autoincrement is True:
+        alter_column_op.kw["autoincrement"] = True
+    elif metadata_col.autoincrement is False:
+        alter_column_op.kw["autoincrement"] = False
+
+    return PriorityDispatchResult.CONTINUE
+
+
+def setup(plugin: Plugin) -> None:
+    plugin.add_autogenerate_comparator(
+        _user_compare_server_default,
+        "column",
+        "server_default",
+        priority=DispatchPriority.FIRST,
+    )
+    plugin.add_autogenerate_comparator(
+        _compare_computed_default,
+        "column",
+        "server_default",
+    )
+
+    plugin.add_autogenerate_comparator(
+        _compare_identity_default,
+        "column",
+        "server_default",
+    )
+
+    plugin.add_autogenerate_comparator(
+        _setup_autoincrement,
+        "column",
+        "server_default",
+    )
+    plugin.add_autogenerate_comparator(
+        _dialect_impl_compare_server_default,
+        "column",
+        "server_default",
+        priority=DispatchPriority.LAST,
+    )

tests/test_autogen_diffs.py
@@ -39,6 +39,7 @@ from sqlalchemy.types import VARBINARY
 from alembic import autogenerate
 from alembic import testing
 from alembic.autogenerate import api
+from alembic.autogenerate.compare.tables import _compare_tables
 from alembic.migration import MigrationContext
 from alembic.operations import ops
 from alembic.testing import assert_raises_message
@@ -57,6 +58,11 @@ from alembic.testing.suite._autogen_fixtures import AutogenFixtureTest
 from alembic.testing.suite._autogen_fixtures import AutogenTest
 from alembic.testing.suite._autogen_fixtures import ModelOne
 from alembic.util import CommandError
+
+if True:
+    from alembic.autogenerate.compare.types import (
+        _dialect_impl_compare_type as _compare_type,
+    )
 
 # TODO: we should make an adaptation of CompareMetadataToInspectorTest that is
 #       more well suited towards generic backends (2021-06-10)
@@ -500,7 +506,7 @@ class AutogenerateDiffTest(ModelOne, AutogenTest, TestBase):
 
     def test_skip_null_type_comparison_reflected(self):
         ac = ops.AlterColumnOp("sometable", "somecol")
-        autogenerate.compare._compare_type(
+        _compare_type(
             self.autogen_context,
             ac,
             None,
@@ -514,7 +520,7 @@ class AutogenerateDiffTest(ModelOne, AutogenTest, TestBase):
 
     def test_skip_null_type_comparison_local(self):
         ac = ops.AlterColumnOp("sometable", "somecol")
-        autogenerate.compare._compare_type(
+        _compare_type(
             self.autogen_context,
             ac,
             None,
@@ -531,7 +537,7 @@ class AutogenerateDiffTest(ModelOne, AutogenTest, TestBase):
             impl = Integer
 
         ac = ops.AlterColumnOp("sometable", "somecol")
-        autogenerate.compare._compare_type(
+        _compare_type(
             self.autogen_context,
             ac,
             None,
@@ -544,7 +550,7 @@ class AutogenerateDiffTest(ModelOne, AutogenTest, TestBase):
         assert not ac.has_changes()
 
         ac = ops.AlterColumnOp("sometable", "somecol")
-        autogenerate.compare._compare_type(
+        _compare_type(
             self.autogen_context,
             ac,
             None,
@@ -567,7 +573,7 @@ class AutogenerateDiffTest(ModelOne, AutogenTest, TestBase):
                     return dialect.type_descriptor(CHAR(32))
 
         uo = ops.AlterColumnOp("sometable", "somecol")
-        autogenerate.compare._compare_type(
+        _compare_type(
             self.autogen_context,
             uo,
             None,
@@ -583,7 +589,7 @@ class AutogenerateDiffTest(ModelOne, AutogenTest, TestBase):
 
         inspector = inspect(self.bind)
         uo = ops.UpgradeOps(ops=[])
-        autogenerate.compare._compare_tables(
+        _compare_tables(
             OrderedSet([(None, "extra"), (None, "user")]),
             OrderedSet(),
             inspector,
@@ -1204,12 +1210,12 @@ class AutogenerateCustomCompareTypeTest(AutogenTest, TestBase):
         my_compare_type.return_value = False
         self.context._user_compare_type = my_compare_type
 
-        diffs = []
         ctx = self.autogen_context
-        diffs = []
-        autogenerate._produce_net_changes(ctx, diffs)
 
-        eq_(diffs, [])
+        uo = ops.UpgradeOps(ops=[])
+        autogenerate._produce_net_changes(ctx, uo)
+
+        eq_(uo.as_diffs(), [])
 
     def test_column_type_modified_custom_compare_type_returns_True(self):
         my_compare_type = mock.Mock()

.pre-commit-config.yaml
@@ -7,7 +7,7 @@ repos:
     -   id: black
 
 -   repo: https://github.com/sqlalchemyorg/zimports
-    rev: v0.6.2
+    rev: v0.7.0
     hooks:
     -   id: zimports
         args:

alembic/__init__.py
@@ -1,4 +1,6 @@
 from . import context
 from . import op
+from .runtime import plugins
 
-__version__ = "1.17.3"
+
+__version__ = "1.18.0"

alembic/autogenerate/api.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import contextlib
+import logging
 from typing import Any
 from typing import Dict
 from typing import Iterator
@@ -17,10 +18,8 @@ from . import compare
 from . import render
 from .. import util
 from ..operations import ops
+from ..runtime.plugins import Plugin
 from ..util import sqla_compat
-
-"""Provide the 'autogenerate' feature which can produce migration operations
-automatically."""
 
 if TYPE_CHECKING:
     from sqlalchemy.engine import Connection
@@ -42,6 +41,10 @@ if TYPE_CHECKING:
     from ..script.base import Script
     from ..script.base import ScriptDirectory
     from ..script.revision import _GetRevArg
+    from ..util import PriorityDispatcher
+
+
+log = logging.getLogger(__name__)
 
 
 def compare_metadata(context: MigrationContext, metadata: MetaData) -> Any:
@@ -304,7 +307,7 @@ class AutogenContext:
 
     """
 
-    dialect: Optional[Dialect] = None
+    dialect: Dialect
     """The :class:`~sqlalchemy.engine.Dialect` object currently in use.
 
     This is normally obtained from the
@@ -326,8 +329,10 @@ class AutogenContext:
 
     """
 
-    migration_context: MigrationContext = None  # type: ignore[assignment]
+    migration_context: MigrationContext
     """The :class:`.MigrationContext` established by the ``env.py`` script."""
+
+    comparators: PriorityDispatcher
 
     def __init__(
         self,
@@ -344,6 +349,19 @@ class AutogenContext:
             raise util.CommandError(
                 "autogenerate can't use as_sql=True as it prevents querying "
                 "the database for schema information"
+            )
+
+        # branch off from the "global" comparators.  This collection
+        # is empty in Alembic except that it is populated by third party
+        # extensions that don't use the plugin system.  so we will build
+        # off of whatever is in there.
+        if autogenerate:
+            self.comparators = compare.comparators.branch()
+            Plugin.populate_autogenerate_priority_dispatch(
+                self.comparators,
+                include_plugins=migration_context.opts.get(
+                    "autogenerate_plugins", ["alembic.autogenerate.*"]
+                ),
             )
 
         if opts is None:
@@ -380,9 +398,8 @@ class AutogenContext:
         self._name_filters = name_filters
 
         self.migration_context = migration_context
-        if self.migration_context is not None:
-            self.connection = self.migration_context.bind
-            self.dialect = self.migration_context.dialect
+        self.connection = self.migration_context.bind
+        self.dialect = self.migration_context.dialect
 
         self.imports = set()
         self.opts: Dict[str, Any] = opts

alembic/autogenerate/compare/__init__.py
@@ -1,0 +1,62 @@
+from __future__ import annotations
+
+import logging
+from typing import TYPE_CHECKING
+
+from . import comments
+from . import constraints
+from . import schema
+from . import server_defaults
+from . import tables
+from . import types
+from ... import util
+from ...runtime.plugins import Plugin
+
+if TYPE_CHECKING:
+    from ..api import AutogenContext
+    from ...operations.ops import MigrationScript
+    from ...operations.ops import UpgradeOps
+
+
+log = logging.getLogger(__name__)
+
+comparators = util.PriorityDispatcher()
+"""global registry which alembic keeps empty, but copies when creating
+a new AutogenContext.
+
+This is to support a variety of third party plugins that hook their autogen
+functionality onto this collection.
+
+"""
+
+
+def _populate_migration_script(
+    autogen_context: AutogenContext, migration_script: MigrationScript
+) -> None:
+    upgrade_ops = migration_script.upgrade_ops_list[-1]
+    downgrade_ops = migration_script.downgrade_ops_list[-1]
+
+    _produce_net_changes(autogen_context, upgrade_ops)
+    upgrade_ops.reverse_into(downgrade_ops)
+
+
+def _produce_net_changes(
+    autogen_context: AutogenContext, upgrade_ops: UpgradeOps
+) -> None:
+    assert autogen_context.dialect is not None
+
+    autogen_context.comparators.dispatch(
+        "autogenerate", qualifier=autogen_context.dialect.name
+    )(autogen_context, upgrade_ops)
+
+
+Plugin.setup_plugin_from_module(schema, "alembic.autogenerate.schemas")
+Plugin.setup_plugin_from_module(tables, "alembic.autogenerate.tables")
+Plugin.setup_plugin_from_module(types, "alembic.autogenerate.types")
+Plugin.setup_plugin_from_module(
+    constraints, "alembic.autogenerate.constraints"
+)
+Plugin.setup_plugin_from_module(
+    server_defaults, "alembic.autogenerate.defaults"
+)
+Plugin.setup_plugin_from_module(comments, "alembic.autogenerate.comments")

alembic/autogenerate/compare/comments.py
@@ -1,0 +1,106 @@
+from __future__ import annotations
+
+import logging
+from typing import Any
+from typing import Optional
+from typing import TYPE_CHECKING
+from typing import Union
+
+from ...operations import ops
+from ...util import PriorityDispatchResult
+
+if TYPE_CHECKING:
+
+    from sqlalchemy.sql.elements import quoted_name
+    from sqlalchemy.sql.schema import Column
+    from sqlalchemy.sql.schema import Table
+
+    from ..api import AutogenContext
+    from ...operations.ops import AlterColumnOp
+    from ...operations.ops import ModifyTableOps
+    from ...runtime.plugins import Plugin
+
+log = logging.getLogger(__name__)
+
+
+def _compare_column_comment(
+    autogen_context: AutogenContext,
+    alter_column_op: AlterColumnOp,
+    schema: Optional[str],
+    tname: Union[quoted_name, str],
+    cname: quoted_name,
+    conn_col: Column[Any],
+    metadata_col: Column[Any],
+) -> PriorityDispatchResult:
+    assert autogen_context.dialect is not None
+    if not autogen_context.dialect.supports_comments:
+        return PriorityDispatchResult.CONTINUE
+
+    metadata_comment = metadata_col.comment
+    conn_col_comment = conn_col.comment
+    if conn_col_comment is None and metadata_comment is None:
+        return PriorityDispatchResult.CONTINUE
+
+    alter_column_op.existing_comment = conn_col_comment
+
+    if conn_col_comment != metadata_comment:
+        alter_column_op.modify_comment = metadata_comment
+        log.info("Detected column comment '%s.%s'", tname, cname)
+
+        return PriorityDispatchResult.STOP
+    else:
+        return PriorityDispatchResult.CONTINUE
+
+
+def _compare_table_comment(
+    autogen_context: AutogenContext,
+    modify_table_ops: ModifyTableOps,
+    schema: Optional[str],
+    tname: Union[quoted_name, str],
+    conn_table: Optional[Table],
+    metadata_table: Optional[Table],
+) -> PriorityDispatchResult:
+    assert autogen_context.dialect is not None
+    if not autogen_context.dialect.supports_comments:
+        return PriorityDispatchResult.CONTINUE
+
+    # if we're doing CREATE TABLE, comments will be created inline
+    # with the create_table op.
+    if conn_table is None or metadata_table is None:
+        return PriorityDispatchResult.CONTINUE
+
+    if conn_table.comment is None and metadata_table.comment is None:
+        return PriorityDispatchResult.CONTINUE
+
+    if metadata_table.comment is None and conn_table.comment is not None:
+        modify_table_ops.ops.append(
+            ops.DropTableCommentOp(
+                tname, existing_comment=conn_table.comment, schema=schema
+            )
+        )
+        return PriorityDispatchResult.STOP
+    elif metadata_table.comment != conn_table.comment:
+        modify_table_ops.ops.append(
+            ops.CreateTableCommentOp(
+                tname,
+                metadata_table.comment,
+                existing_comment=conn_table.comment,
+                schema=schema,
+            )
+        )
+        return PriorityDispatchResult.STOP
+
+    return PriorityDispatchResult.CONTINUE
+
+
+def setup(plugin: Plugin) -> None:
+    plugin.add_autogenerate_comparator(
+        _compare_column_comment,
+        "column",
+        "comments",
+    )
+    plugin.add_autogenerate_comparator(
+        _compare_table_comment,
+        "table",
+        "comments",
+    )

commit f532224351f75467c6a90962f1edbb0abd162f41

dont compare server defaults if user FN returns non-None
fixed regression caused by e532a7e39cb6b0e91fbe045778f3
where we started continuing default server default comparison
even if user defined function returned False.

Fixes: #1777
Change-Id: I99fe8a10bc58a83aae1f1fae48b3b151a4db64c6

alembic/autogenerate/compare/server_defaults.py
@@ -227,6 +227,11 @@ def _user_compare_server_default(
             cname,
         )
         return PriorityDispatchResult.STOP
+    elif is_diff is False:
+        # if user compare server_default returns False and not None,
+        # it means "dont do any more server_default comparison"
+        return PriorityDispatchResult.STOP
+
     return PriorityDispatchResult.CONTINUE
 
 

tests/test_autogen_diffs.py
@@ -884,6 +884,63 @@ class CompareServerDefaultTest(TestBase):
 
         assert not diff
 
+    @testing.combinations(
+        (False, "different", False),
+        (True, "original", True),
+        (None, "different", True),
+        id_="naa",
+        argnames="return_value,new_default,expect_diff",
+    )
+    def test_user_compare_server_default_return_values(
+        self, return_value, new_default, expect_diff, connection, metadata
+    ):
+        """Test user compare_server_default callable return values.
+
+        This is a regression test for #1777 where the plugin refactoring
+        broke the handling of False return values from user-defined
+        compare_server_default callables.
+
+        - False: stop comparison, no diff detected
+        - True: diff detected regardless of actual difference
+        - None: continue to default comparison logic
+        """
+
+        def my_compare_server_default(
+            context,
+            inspected_col,
+            metadata_col,
+            inspected_default,
+            metadata_default,
+            rendered_metadata_default,
+        ):
+            return return_value
+
+        t1 = Table(
+            "t1",
+            metadata,
+            Column("x", VARCHAR(30), server_default=text("'original'")),
+        )
+        t1.create(connection)
+
+        new_metadata = MetaData()
+        Table(
+            "t1",
+            new_metadata,
+            Column("x", VARCHAR(30), server_default=text(f"'{new_default}'")),
+        )
+
+        mc = MigrationContext.configure(
+            connection,
+            opts={"compare_server_default": my_compare_server_default},
+        )
+
+        diff = api.compare_metadata(mc, new_metadata)
+        if expect_diff:
+            eq_(len(diff), 1)
+            eq_(diff[0][0][0], "modify_default")
+        else:
+            assert not diff
+
 
 class CompareMetadataToInspectorTest(TestBase):
     __backend__ = True